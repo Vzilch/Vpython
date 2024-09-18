92131e0c4b66b46eff2177e1dee7e3e168956597
Vzilch <kavistar83@gmail.com>
2024-09-18 15:02:03 +0530

    Add files via upload

--- V.py
+++ V.py
@@ -1,6 +1,9 @@
 
+#I>P>O is input then processing then output
+#the class methods are sequenced in the above formate with only one exception
+#G, indicates that the function makes/generates something and returns it comes P
 
-#to do - .out(), .println(), graph(), .treechart()
+import time
 
 class ipo:
     
@@ -8,7 +11,7 @@ class ipo:
         self.l = l
         self.w = w
 
-    # I, inputs
+    # I, reurns a list of inputs
     def ins(self, n = 1, it = 0):
         o = list(range(n))
         while n != 0:
@@ -22,151 +25,27 @@ class ipo:
     def cls(self):
         print("\n" * (self.l - 1))
 
-    #Nav function for IPO, I>P
+    #Nav function for IPO, I>P>O
     def usrwait(self, prompt = "Continue"):
         a = input(prompt)
         if a == "quitp":
+            print("\n\nprogram off")
             return False
         else:
             return a
 
-    # Breaksdown text into chartypes, P
-    def breakdown(self, string = ""):
-        convention = ["base10", "letter", "bodmas"]
-        nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-        base10_str = "0123456789"
-        letters_str = " AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"
-        bodmas_str = "()^/*+-"
-        
-        gen = []
-        stringlen = len(string)
-        while stringlen > 0:
-            gen += [None]
-            stringlen -= 1
-        
-        oindex = 0
-        for i in string:
-            index = 0
-            for x in base10_str:
-                if i == x:
-                    gen[oindex] = [nums[index], index, "number"]
-                    oindex += 1
-                index += 1
-
-            index = 0
-            for x in letters_str:
-                if i == x:
-                    gen[oindex] = [i, index, "ascii"]
-                    oindex += 1
-                index += 1
-
-            index = 0
-            for x in bodmas_str:
-                if i == x:
-                    gen[oindex] = [i, index, "bodmas"]
-                    oindex += 1
-                index += 1
-        return gen
-
-    #list generator, G
-    def nlist(self, length = 1, depth = 1, ns = 1):
-        a = []
-        b = []
-        copy = ns
-
-        #set number of "none" vals in each indice
-        while ns > 0:
-            a += [None]
-            ns -= 1
-        #set depth/dimensions/brackets
-        while depth - 1 > 0:
-            a = [a]
-            depth -= 1
-        #length of list
-        while length > 0:
-            b += a
-            length -= 1
-        
-        return b
-
-    #makes list for window sized outputs, G
-    def windowlist(self, filler = " ", typ = 0):
-        out = []
-        count = self.w * self.l + self.l
-        width = self.w + 1
-        it = 0
-        
-        if typ == 1:
-            while count > 0:
-                if it % width == 0:
-                    out += "\n"
-                else:
-                    out += [None]
-                it += 1
-                count -= 1
-        
-        else:
-            while count > 0:
-                if it % width == 0:
-                    out += "\n"
-                else:
-                    out += filler
-                it += 1
-                count -= 1
-        out[0] = "Window\n"
-        return out
-
-    #for adding char in o arr, p
-    def putone(self, char, rc = [1, 1], arr = []):
-        pos = rc[0] + (rc[1] - 1) * (self.w + 1)
-        arr[pos] = char[0]
-        return arr
-
-    #for adding line in O array, P
-    def posput(self, iterable = [], rc = [2, 2], arr = []):
-        pos = rc[0] + (rc[1] - 1) * (self.w + 1)
-        #self,w + 1 because, newlines in prev func
-        end = len(arr)
-        x = 0
-        n = len(iterable)
-        while n > 0:
-            if pos % (self.w + 1) == 0:
-                pos += 1
-            try:
-                arr[pos] = iterable[x]
-            except IndexError:
-                break
+    #nested list generator, P>G
+    def nlist(self, n = 2):
+        n += 1
+        out = [None] * n
+        x = 1
+        print([out[x]])
+        while x < n:
+            out[x] = [out[x - 1]]
             x += 1
-            pos += 1
-            n -= 1
+        return out[n - 1]
 
-    def vremove(self, iterable = " ", rc = [1, 1], arr = []):
-        pos = rc[0] + (rc[1] - 1) * (self.w + 1)
-        end = len(arr)
-
-        for i in iterable:
-            if pos > end:
-                pos = 5
-            arr[pos] = " "
-            pos += 1
-            if pos % (self.w + 1) == 0:
-                pos += 1
-            
-        return arr
-
-
-
-    #Adds up items in a list, P
-    def windowstr(self, inlist = [None], filler = " "):
-        out = ""
-        for i in inlist:
-            try:
-                out += i
-            except TypeError:
-                out += filler
-        return out
-
-    #generates a list of lists of chars, G
+    #generates a list of lists of whitespaces, P>G
     def awin(self):
         l = self.l
         w = self.w
@@ -175,25 +54,6 @@ class ipo:
             out += [[" "] * w]
             l -= 1
         return out
-
-    #concatenates list if lists of chars into one string, P>O
-    def o(self, a = [], typ = 0):
-        out = ""
-        index = 0
-        for i in a:
-            for x in i:
-                out += x
-                
-            if index == self.l - 1:
-                break
-            
-            out += "\n"
-            index += 1
-        
-        if typ == 1:
-            return out
-        else:
-            print(out)
 
     #adds a sequence of chars to a list of lists, P
     def vinsert(self, iterable = "", x = 1, y = 1, lis = [], typ = "", fill = " "):
@@ -232,7 +92,7 @@ class ipo:
         else:
             return [a, off]
 
-    # queue displacement cycle, P
+    #displaces all items in a ques by n indexes, P
     def discyc(self, a = [], times = 1, typ = 0):
         x = 0
         lena = len(a)
@@ -256,7 +116,29 @@ class ipo:
         else:
             return None
 
-    
+    #concatenates list if lists of chars into one string, P>O
+    def o(self, a = [], wait = 0.00, typ = 0):
+        out = ""
+        index = 0
+        for i in a:
+            for x in i:
+                out += x
+                
+            if index == self.l - 1:
+                break
+            
+            out += "\n"
+            index += 1
+        
+        if typ == 1:
+            return out
+        else:
+            print(out)
+            if wait > 0.00:
+                time.sleep(wait)
+
+    #this is the example of a bad function as it does two things not one and is messed up
+    #takes a set of coordinates as input and returns a lists of data (max val xcoord and ycoord)
     def incoord(self, n = 1, prompt = "Enter coordinate\t"):
             xc = list(range(n+1))
             yc = list(range(n+1))
@@ -297,9 +179,65 @@ class ipo:
                         it += 1
                     
             return [clist, xc, yc, [xc[0], yc[0]]]
-                
-            
-    def treechart(self):
-        print("incomplete")
 
 
+#for sorking on strings
+class vstring:
+
+    def __init__(self, ins):
+        self.ins = ins
+
+    def breakdown(self, string = ""):
+        convention = ["base10", "letter", "bodmas"]
+        nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        base10_str = "0123456789"
+        letters_str = " AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"
+        bodmas_str = "()^/*+-"
+        
+        gen = []
+        stringlen = len(string)
+        while stringlen > 0:
+            gen += [None]
+            stringlen -= 1
+        
+        oindex = 0
+        for i in string:
+            index = 0
+            for x in base10_str:
+                if i == x:
+                    gen[oindex] = [nums[index], oindex, "number"]
+                    oindex += 1
+                index += 1
+
+            index = 0
+            for x in letters_str:
+                if i == x:
+                    gen[oindex] = [i, oindex, "ascii"]
+                    oindex += 1
+                index += 1
+
+            index = 0
+            for x in bodmas_str:
+                if i == x:
+                    gen[oindex] = [i, oindex, "bodmas"]
+                    oindex += 1
+                index += 1
+        return gen
+
+    
+    def getwrds(selfstring = ""): 
+        bre = breakdown(string)
+        wrds = []
+        x = 0
+        wrd = ""
+        for i in string:
+            if bre[x][2] == "ascii":
+                if bre[x][0] == " ":
+                    wrds += [wrd]
+                    #print(x)
+                    wrd = ""
+                else:
+                    wrd += bre[x][0]
+            x += 1
+        wrds += [wrd]
+        return wrds
--- example.py
+++ example.py
@@ -1,11 +1,11 @@
 import V
 import time
 
-def afunc():
+def arandom():
     scr = V.ipo()
-    out = ""
+    out = "g"
     arr = scr.windowlist()
-    string = "this is example code..."
+    string = "sample code..."
     a = 20
     b = 30
     for i in range(40):
@@ -16,10 +16,25 @@ def afunc():
         time.sleep(0.05)
         b += 1
 
-
+#example program with Vpy module
+        #startin v module objects and other basics
 z = V.ipo()
-a = z.awin()
-my = "hi bye"
-z.vinserts(my, 1, 1, a)
-z.o(a)
+win = z.awin()
+swich = True
+
+# code
+while swich == True:
+    z.cls()
+    
+    x = int(input("enter x\t"))
+    y = int(input("enter y\t"))
+    filler = "*" * (x * y)
+
+    swich = z.usrwait() #if user enters quitp it terminates the loop
+    z.vinsert(filler, x, y, win)
+    z.o(win, 0.8)
+
+
+
+    
 
