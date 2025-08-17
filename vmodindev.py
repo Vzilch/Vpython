7eb1e07cbdd7f8941aae949e78e449403530f38a
Vzilch <kavistar83@gmail.com>
2025-08-17 18:37:52 +0530

    Update vmodindev.py

--- vmodindev.py
+++ vmodindev.py
@@ -6,7 +6,9 @@ class window:
         def __init__(self, x, y):
                 self.x = x
                 self.y = y
-                self.arr = [[["","","",""]] * self.x] * self.y  #note: rgb+d           
+                self.arr = [[["","","",""]] * self.x] * self.y  #note: rgb+d   
+
+        #hex format: #00000000, (rgb+d)
         def update_pt(self, x = int, y = int, hex = str):
                 self.arr[y][x][0] = hex[1:3]
                 self.arr[y][x][1] = hex[3:5]
