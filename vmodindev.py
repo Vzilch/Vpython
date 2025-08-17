60786800a9348399e0007c610a5bb9fd01754cd8
Vzilch <kavistar83@gmail.com>
2025-08-17 18:23:11 +0530

    Create vmodindev.py

---
+++ vmodindev.py
@@ -0,0 +1,15 @@
+class window:
+        def __init__(self, x, y):
+                self.x = x
+                self.y = y
+                self.arr = [[["","","",""]] * self.x] * self.y
+                                                                                def scr(self):
+                return [[["","","",""]] * self.x] * self.y              
+        def update_pt(self, x = int, y = int, hex = str):
+                self.arr[y][x][0] = hex[1:3]
+                self.arr[y][x][1] = hex[3:5]
+                self.arr[y][x][2] = hex[5:7]
+                self.arr[y][x][3] = hex[7:9]
+
+        def hexof(self, x = int, y = int):
+                return "#" + "".join(val for val in self.arr[y][x])
